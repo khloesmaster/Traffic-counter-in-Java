4ee261178b338f331e4ea477d827cef30f7070af
Fábián Kristóf <f-kristof@hotmail.com>
2015-05-25 14:59:30 +0300

    minor modifications

--- src/test/java/hu/unideb/fksz/VideoProcessorTest.java
+++ src/test/java/hu/unideb/fksz/VideoProcessorTest.java
@@ -2,19 +2,45 @@ package hu.unideb.fksz;
 
 import org.junit.Test;
 import org.opencv.core.Core;
-
+import static org.junit.Assert.*;
 
 public class VideoProcessorTest {
-	static
-	{
+	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 	VideoProcessor testProcessor = new VideoProcessor();
-	
+
 	@Test
-	public void testInitVideo()
-	{
-		//testProcessor.initVideo(VideoProcessorTest.class.getResource("/video/Debrecen_Egyetemsgt.mp4").getPath());
+	public void testInitVideo() {
+
+		assertEquals(
+				0,
+				testProcessor.initVideo(VideoProcessorTest.class.getResource(
+						"/Debrecen_Egyetemsgt.mp4").getPath()));
+
 	}
 
+	@Test
+	public void testGetFPS() {
+		assertNotEquals(0, testProcessor.getFPS());
+	}
+
+	@Test
+	public void getVideoCap() {
+		assertNotNull("Not null!", testProcessor.getVideoCap());
+	}
+
+	@Test
+	public void testGetControlPointsHeight() {
+		assertNotEquals(0, testProcessor.getControlPointsHeight());
+	}
+
+	@Test
+	public void testGetHeightOfAControlPoint() {
+		assertNotEquals(0, testProcessor.getHeightOfAControlPoint());
+
+	}
+
+
+	
 }
