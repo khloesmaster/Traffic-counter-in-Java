2d357e6bb7a89d7e3d4ffc8c2ad8e17821056a93
Fabian Kristof <f-kristof@hotmail.com>
2016-04-27 00:58:19 +0200

    OpenCV 3.0 gold natives added.

--- src/main/java/hu/unideb/fksz/Main.java
+++ src/main/java/hu/unideb/fksz/Main.java
@@ -10,18 +10,17 @@ package hu.unideb.fksz;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-
 
 
 import org.opencv.core.Core;
@@ -34,20 +33,18 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 
-public class Main extends Application 
+public class Main extends Application
 {
 
 	@Override
-	public void start(Stage primaryStage) 
+	public void start(Stage primaryStage)
 	{
-		try 
+		try
 		{
-			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/TrafficCounterWindow.fxml"));
 			AnchorPane root = (AnchorPane) loader.load();
 			Scene scene = new Scene(root);
-			
-			
+
 			primaryStage.setTitle("Traffic Counter");
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
@@ -55,21 +52,19 @@ public class Main extends Application
 			primaryStage.show();
 
 			TrafficCounterController controller = loader.getController();
-			
+
 			controller.init();
 			controller.setStage(primaryStage);
-		} 
-		catch (Exception e) 
+		}
+		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-				
 		launch(args);
 	}
 }
